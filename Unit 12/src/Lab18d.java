7ccf5ecd144e5be1d4a15877beed8b2200daa308
MinaBurns <minaburns@outlook.com>
2018-03-19 11:44:01 -0700

    Unit 12

--- Unit 12/src/Lab18d.java
+++ Unit 12/src/Lab18d.java
@@ -14,11 +14,32 @@ public class Lab18d
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("lab18d.dat"));
+		Scanner file = new Scanner(new File("C:\\Users\\burnsc6389\\Documents\\GitHub\\APCSA-V3\\Unit12-2016(1)\\Unit12-2016\\Unit12-Assignments\\lab18d.dat"));
 
 		int size = file.nextInt();
 		file.nextLine();
+		Word[] wordList = new Word[size];
+		for(int i = 0; i<size; i++)
+		{
+			wordList[i] = new Word(file.nextLine());
+		}
+		for(int i = 0; i<size; i++)
+		{
+			for (int j = 0; j< size-1; j++)
+			{
+				if (wordList[j].compareTo(wordList[j+1])==1)
+				{
+					Word one = wordList[j+1];
+					wordList[j+1] = wordList[j];
+					wordList[j] = one;
+				}
+			}
+		}
 		
+		for(int i = 0; i<size; i++)
+		{
+			System.out.println(wordList[i].toString());
+		}
 
 
 
