5ebf60882a1d60fb17c837ddf35e8de36ff5d1d1
MinaBurns <BurnsC6389@sduhsd.lan>
2018-03-22 11:44:42 -0700

    Unit 14

--- Unit 14/src/Lab21i.java
+++ Unit 14/src/Lab21i.java
@@ -17,5 +17,31 @@ public class Lab21i
 		int length = file.nextInt();
 		String s = file.nextLine();
 		Maze test = new Maze(length, s);
+		test.toString();
+		
+		int length2 = file.nextInt();
+		String s2 = file.nextLine();
+		Maze test2 = new Maze(length2, s2);
+		test2.toString();
+		
+		int length3 = file.nextInt();
+		String s3 = file.nextLine();
+		Maze test3 = new Maze(length3, s3);
+		test3.toString();
+		
+		int length4 = file.nextInt();
+		String s4 = file.nextLine();
+		Maze test4 = new Maze(length4, s4);
+		test4.toString();
+		
+		int length5 = file.nextInt();
+		String s5 = file.nextLine();
+		Maze test5 = new Maze(length5, s5);
+		test5.toString();
+		
+		int length6 = file.nextInt();
+		String s6 = file.nextLine();
+		Maze test6 = new Maze(length6, s6);
+		test6.toString();
 	}
 }
