bbaf03b4000caca1d04645f97a6fe5cc67051f19
MinaBurns <BurnsC6389@sduhsd.lan>
2018-03-29 09:30:51 -0700

    Pong

--- Unit 15/src/Block.java
+++ Unit 15/src/Block.java
@@ -18,57 +18,109 @@ public class Block implements Locatable
 
 	public Block()
 	{
-
+		xPos = 0;
+		yPos = 0;
+		width = 0;
+		height = 0;
+		color = Color.white;
 
 	}
 
 	//add other Block constructors - x , y , width, height, color
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public Block (int x, int y, int width, int height, Color c)
+	{
+		setXPos(x);
+		setYPos(y);
+		setWidth(width);
+		setHeight(height);
+		setColor(c);
+	}
 	
 	
    //add the other set methods
    
+	public void setXPos(int x)
+	{
+		xPos = x;
+	}
+	
+	public void setYPos(int y)
+	{
+		yPos = y;
+	}
+	
+	public void setWidth(int w)
+	{
+		width = w;
+	}
+	
+	public void setHeight(int h)
+	{
+		height = h;
+	}
 
    public void setColor(Color col)
    {
-
+	   color = col;
 
    }
+   
+   public int getX()
+	{
+		return xPos;
+	}
+	
+	public int getY()
+	{
+		return yPos;
+	}
+	
+	public int getWidth()
+	{
+		return width;
+	}
+	
+	public int getHeight()
+	{
+		return height;
+	}
+
+  public Color getColor()
+  {
+	  return color;
+
+  }
 
    public void draw(Graphics window)
    {
    	//uncomment after you write the set and get methods
-      //window.setColor(color);
-      //window.fillRect(getX(), getY(), getWidth(), getHeight());
+      window.setColor(color);
+      window.fillRect(getX(), getY(), getWidth(), getHeight());
    }
 
    public void draw(Graphics window, Color col)
    {
-
+	   window.setColor(col);
+	   window.fillRect(getX(), getY(), getWidth(), getHeight());
 
    }
    
-	public boolean equals(Object obj)
+	public boolean equals(Block obj)
 	{
-
-
-
+		if(xPos==obj.getX() && getY()==obj.getY() && getWidth() == obj.getWidth() && getHeight() == obj.getHeight() && getColor() == obj.getColor())
+		{
+			return true;
+		}
 
 		return false;
 	}   
 
-   //add the other get methods
-    
+	public String toString()
+	{
+		String output = "";
+		
+		return output;
+	}
 
    //add a toString() method  - x , y , width, height, color
 }
